0e2f635f7f6520ffb400b122408238aef31da3fb
chantal <chantal@DESKTOP-C9KR8GN>
2019-04-16 13:35:54 +0200

    Volunteer Model Added

---
+++ Models/Volunteer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charity_management_system.Models
+{
+    class Volunteer : Employee
+    {
+        public bool currentlyWorking;
+        public Volunteer(int SSN, string Name, string addressLine1, string city, string governorate, int branchID, bool currentlyWorking) : base(SSN, Name, addressLine1, city, governorate, branchID)
+        {
+            this.currentlyWorking = currentlyWorking;
+        }
+    }
+}
