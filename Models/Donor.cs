cdf2af6242728eefb84ca3da12cd2291b31b55b1
chantal <chantal@DESKTOP-C9KR8GN>
2019-04-16 12:26:19 +0200

    Implemented Campaign and Donor Models

---
+++ Models/Donor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charity_management_system.Models
+{
+    class Donor
+    {
+        public int donorID;
+        public String donorName;
+        public String donorMobile;
+        public String donorLine1;
+        public String donorLine2;
+        public String donorGovernrate;
+        public String donorCity;
+
+        public Donor()
+        {
+            donorID = 0;
+            donorName = "";
+            donorMobile = "";
+            donorLine1 = "";
+            donorLine2 = "";
+            donorGovernrate = "";
+            donorCity = "";
+        }
+    }
+}
