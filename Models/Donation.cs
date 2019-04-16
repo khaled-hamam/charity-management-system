e6fdebd9c184b037ee211b536b458610fdcfedce
mariamkamel <mariammkamel98@gmail.com>
2019-04-16 12:59:48 +0200

    donation, donation repo added

---
+++ Models/Donation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace charity_management_system.Models
+{
+    class Donation
+    {
+        public Donor donor;
+        public Campaign campaign;
+        public DateTime date;
+        public String type;
+        public float value; 
+        
+        public Donation(DateTime date, String type)
+        {
+            this.date = date;
+            this.type = type;
+        }
+    }
+}
