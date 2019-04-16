fb9707215b2f40057c181787ea05288214f85c31
mariamkamel <mariammkamel98@gmail.com>
2019-04-16 13:28:56 +0200

    paid employee model added

---
+++ Models/PaidEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using charity_management_system.Models;
+
+namespace charity_management_system.Models
+{
+    class PaidEmployee : Employee
+    {
+        float salary;
+        Department department;
+
+        public PaidEmployee(int SSN, string Name, string addressLine1, string addressLine2, string Governorate, int branchID, float salary) : base(SSN, Name, addressLine1, addressLine2, Governorate, branchID)
+        {
+            this.salary = salary;
+        }
+    }
+}
