6a80e4ef8f98a05f7c7c87b1e7ac341aab143c6d
karim <karim.abdelghaffar98@gmail.com>
2019-04-16 13:25:34 +0200

    Department class

---
+++ Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charity_management_system.Models
+{
+    class Department
+    {
+        String name;
+        public String Name { get { return this.name; } private set { this.name = value; } }
+
+        public Department()
+        {
+            this.name = "";
+        }
+        public Department(String name)
+        {
+            this.name = name;
+        }
+    }
+}
