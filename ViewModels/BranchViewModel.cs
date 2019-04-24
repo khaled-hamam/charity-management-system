40c8fa719d4bd8a577d06a894579891654bbf790
chantal <chantal@DESKTOP-C9KR8GN>
2019-04-24 18:09:35 +0200

    branch added

---
+++ ViewModels/BranchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using charity_management_system.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using charity_management_system.Repositories;
+
+namespace charity_management_system.ViewModels
+{
+    class BranchViewModel : Screen
+    {
+        public List<Branch> branches { get; set; }
+        private IRepository<Branch> _repository;
+        public BranchViewModel()
+        {
+            _repository = new BranchRepo();
+            branches = new List<Branch>();
+            branches = _repository.findAll();
+        }
+    }
+}
