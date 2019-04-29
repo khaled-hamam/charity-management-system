ee3653ffb4814b9f27331bf363c12966ff01bf9c
mariamkamel <mariammkamel98@gmail.com>
2019-04-30 01:59:24 +0200

    paid employee delete/update functions added

--- Views/Components/EmployeeCardView.xaml.cs
+++ Views/Components/EmployeeCardView.xaml.cs
@@ -18,20 +18,54 @@ namespace charity_management_system.Views.Components
     /// </summary>
     public partial class EmployeeCardView : UserControl
     {
+        DepartmentRepo dr = new DepartmentRepo();
+        BranchRepo br = new BranchRepo();
         public EmployeeCardView()
         {
             InitializeComponent();
+            foreach (Branch branch in br.findAll())
+            {
+                EmployeeBranchComboBox.Items.Add(branch.id);
+            }
+            foreach (Department dep in dr.findAll())
+            {
+                EmployeeDepartmentComboBox.Items.Add(dep.name);
+            }
+            
         }
         private void UpdateEmployeeClick(object sender, RoutedEventArgs e)
         {
+            PaidEmployeeRepo employeeRepo = new PaidEmployeeRepo();
+            PaidEmployee employee = ((EmployeeCardViewModel)this.DataContext).employee;
+
+            char empGender;
+            if (employeeGender.Text.ToString() == "Male")
+                empGender = 'M';
+            else
+                empGender = 'F';
+
+            employee.name = nameTextBox.Text;
+            employee.mobile = mobileTextBox.Text;
+            employee.email = emailTextBox.Text;
+            employee.birthDate = birthdayBox.SelectedDate.Value.Date;
+            employee.gender = empGender;
+            employee.addressLine1 = line1TextBox.Text;
+            employee.addressLine2 = line2TextBox.Text;
+            employee.city = cityTextBox.Text;
+            employee.governorate = governorateTextBox.Text;
+            employee.branch = new Branch { id = int.Parse(EmployeeBranchComboBox.Text.ToString()) };
+            employee.salary = int.Parse(salaryTextBox.Text);
+            employee.department = new Department(EmployeeDepartmentComboBox.Text.ToString());
+            employee.name = nameTextBox.Text;
+
+            employeeRepo.update(((EmployeeCardViewModel)this.DataContext).employee);
         }
 
         private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
         {
-            PaidEmployeeRepo emp = new PaidEmployeeRepo();
+            PaidEmployeeRepo employeeRepo = new PaidEmployeeRepo();
 
-            Console.WriteLine(((EmployeeCardViewModel)this.DataContext).employee.name);
-            emp.delete(((EmployeeCardViewModel)this.DataContext).employee);
+            employeeRepo.delete(((EmployeeCardViewModel)this.DataContext).employee);
             EmployeeDataStore.instance.data.Remove(((EmployeeCardViewModel)this.DataContext).employee);
         }
     }
